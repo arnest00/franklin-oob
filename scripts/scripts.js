0c41358590aa34c90b784c91f23f048d83076d2c
zastrow <philip@zastrow.co>
2022-10-28 09:36:34 -0400

    feat: Add block variations test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,6 +27,17 @@ function buildHeroBlock(main) {
   }
 }
 
+function fixHeading(main) {
+  const h1 = main.querySelector('h1');
+  const picture = main.querySelector('picture');
+  // eslint-disable-next-line no-bitwise
+  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+    const section = document.createElement('div');
+    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    main.prepend(section);
+  }
+}
+
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
